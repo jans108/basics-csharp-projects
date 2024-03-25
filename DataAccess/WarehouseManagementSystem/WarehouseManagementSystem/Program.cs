bfd0f3607854821ca5da388bb1dd9d925a394504
Maciej Broda <maciejbroda01@gmail.com>
2024-03-25 21:22:42 +0100

    Creating a Connection

---
+++ DataAccess/WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+
+var connectionString =
+    "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+    "Initial Catalog=WarehouseManagement;" +
+    "Integrated Security=True;";
+
+using SqlConnection connection = new(connectionString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.Data.SqlClient;
+//using WarehouseManagementSystem;
+//using Microsoft.EntityFrameworkCore;
+//using Warehouse.Data.SQLite;
+//using Order = Warehouse.Data.SQLite.Order;
+//using LineItem = Warehouse.Data.SQLite.LineItem;
+//using Customer = Warehouse.Data.SQLite.Customer;
+//using Item = Warehouse.Data.SQLite.Item;
+//using ShippingProvider = Warehouse.Data.SQLite.ShippingProvider;
+//using Warehouse = Warehouse.Data.SQLite.Warehouse;
+
+//using var context = new WarehouseSQLiteContext();
+
+//var firstCustomer = context.Customers.First();
+//Order newOrder = new()
+//{
+//    Id = Guid.NewGuid(),
+//    LineItems = new LineItem[]
+//    {
+//        new()
+//    {
+//            Id = Guid.NewGuid(),
+//            Item = context.Items.First(),
+//            Quantity = 1
+//    }
+//    },
+//    ShippingProvider = context.ShippingProviders.First(),
+//    Customer = firstCustomer
+//};
+
+//context.Orders.Add(newOrder);
+//context.SaveChanges();
+//Console.WriteLine("Order added!");
