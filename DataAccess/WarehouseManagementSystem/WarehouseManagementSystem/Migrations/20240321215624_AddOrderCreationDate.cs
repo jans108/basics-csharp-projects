d1b9b77e0a30cf2e01b696f6385b9a03adc70904
Maciej Broda <maciejbroda01@gmail.com>
2024-03-28 23:04:13 +0100

    Extending the Generic Repository

---
+++ DataAccess/WarehouseManagementSystem/WarehouseManagementSystem/Migrations/20240321215624_AddOrderCreationDate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseManagementSystem.Migrations
+{
+    public partial class AddOrderCreationDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Orders",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: DateTimeOffset.Now);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Orders");
+        }
+    }
+}
