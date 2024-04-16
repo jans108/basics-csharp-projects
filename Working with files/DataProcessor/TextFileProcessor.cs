6f6beea67e1b9df0db3a0b15ea7897fdf9bc8cdd
Maciej Broda <maciejbroda01@gmail.com>
2024-04-16 23:37:38 +0200

    Reading and Writing Text Files with String Arrays

--- Working with files/DataProcessor/TextFileProcessor.cs
+++ Working with files/DataProcessor/TextFileProcessor.cs
@@ -20,8 +20,22 @@ public class TextFileProcessor
     public void Process()
     {
         // Using read all text
-        string originalText = File.ReadAllText(InputFilePath);
-        string processedText = originalText.ToUpperInvariant();
-        File.WriteAllText(OutputFilePath, processedText);
+        //string originalText = File.ReadAllText(InputFilePath);
+        //string processedText = originalText.ToUpperInvariant();
+        //File.WriteAllText(OutputFilePath, processedText);
+
+        // Using read all lines
+        try
+        {
+            string[] lines = File.ReadAllLines(InputFilePath);
+            lines[1] = lines[1].ToUpperInvariant(); // Assumes there is a line 2 in the file
+            File.WriteAllLines(OutputFilePath, lines);
+        }
+        catch (IOException ex)
+        {
+            //Log / retry
+            Console.WriteLine(ex);
+            throw;
+        }
     }
 }
