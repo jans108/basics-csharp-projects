038032bb18992c6ee43f0c6e304808cd0a47fa64
Maciej Broda <maciejbroda01@gmail.com>
2024-04-20 23:59:00 +0200

    Asserting Mock File Output with Approval Tests

--- Working with files/DataProcessor.Tests/CsvFileProcessorShould.cs
+++ Working with files/DataProcessor.Tests/CsvFileProcessorShould.cs
@@ -1,3 +1,5 @@
+using ApprovalTests;
+using ApprovalTests.Reporters;
 using System.IO.Abstractions.TestingHelpers;
 using System.Text;
 
@@ -46,5 +48,43 @@ public class CsvFileProcessorShould
         Assert.Equal("43,200002,1", lines[2]);
         Assert.Equal("44,300003,5", lines[3]);
 
+    }
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void OutputProcessedOrderCsvData_ApprovalTests()
+    {
+        const string inputDir = @"c:\root\in";
+        const string inputFileName = "myfile.csv";
+        var inputFilePath = Path.Combine(inputDir, inputFileName);
+
+        const string outputDir = @"c:\root\out";
+        const string outputFileName = "myfileout.csv";
+        var outputFilePath = Path.Combine(outputDir, outputFileName);
+
+        var csvLines = new StringBuilder();
+        csvLines.AppendLine("OrderNumber,CustomerNumber,Description,Quantity");
+        csvLines.AppendLine("42, 100001, Shirt, II");
+        csvLines.AppendLine("43, 200002, Shorts, I");
+        csvLines.AppendLine("@ This is a comment");
+        csvLines.AppendLine("");
+        csvLines.Append("44, 300003, Cap, V");
+
+        var mockInputFile = new MockFileData(csvLines.ToString());
+
+        var mockFileSystem = new MockFileSystem();
+        mockFileSystem.AddFile(inputFilePath, mockInputFile);
+        mockFileSystem.AddDirectory(outputDir);
+
+        var sut = new CsvFileProcessor(inputFilePath,
+                                       outputFilePath,
+                                       mockFileSystem);
+
+        sut.Process();
+
+        Assert.True(mockFileSystem.FileExists(outputFilePath));
+
+        MockFileData processedFile = mockFileSystem.GetFile(outputFilePath);
+
+        Approvals.Verify(processedFile.TextContents);
     }
 }
