b5d8b66105fe200ccb72b7148101db053c150128
Maciej Broda <maciejbroda01@gmail.com>
2024-05-23 23:13:17 +0200

    Adding Conditional Code for Different Targets

--- Class Libraries in C#/Globomantics.Math/Calculator.cs
+++ Class Libraries in C#/Globomantics.Math/Calculator.cs
@@ -6,5 +6,19 @@
         {
             return a + b;
         }
+
+
+        public static string AsHex(int a)
+        {
+            var hex = a.ToString("X");
+#if NET8_0
+            return $"{hex} from .NET 8";
+#elif NET461
+            return $"{hex} from .NET Framework 4.6.1";
+#else
+            return $"{hex} from .NET Standard 2.0";
+#endif
+
+        }
     }
 }
