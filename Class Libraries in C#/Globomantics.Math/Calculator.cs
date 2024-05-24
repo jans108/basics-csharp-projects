0c2456327fdd5afa98ccd180293a2aa1ce19cfd7
Maciej Broda <maciejbroda01@gmail.com>
2024-05-24 18:06:36 +0200

    Preventing Bugs if New Targets Are Added

--- Class Libraries in C#/Globomantics.Math/Calculator.cs
+++ Class Libraries in C#/Globomantics.Math/Calculator.cs
@@ -1,4 +1,6 @@
-﻿namespace Globomantics.Math
+﻿using System;
+
+namespace Globomantics.Math
 {
     public static class Calculator
     {
@@ -14,9 +16,25 @@
 #if NET8_0
             return $"{hex} from .NET 8";
 #elif NET461_OR_GREATER
-            return $"{hex} from .NET Framework 4.7.2";
+            return $"{hex} from .NET Framework 4.6.1 or greater";
 #elif NETSTANDARD2_0
             return $"{hex} from .NET Standard 2.0";
+#endif
+
+        }
+
+        public static void WriteAsHex(int a)
+        {
+            string hex = a.ToString("X");
+
+#if NET8_0
+            Console.WriteLine($"{hex} from .NET 8");
+#elif NET461_OR_GREATER
+            Console.WriteLine($"{hex} from .NET Framework 4.6.1 or greater");
+#elif NETSTANDARD2_0
+            Console.WriteLine($"{hex} from .NET Standard 2.0");
+#else
+            throw new System.PlatformNotSupportedException();
 #endif
 
         }
