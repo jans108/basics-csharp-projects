ef96fec73cbe421229411bbd5cfe4436b0bfa88d
Maciej Broda <maciejbroda01@gmail.com>
2024-06-03 19:55:19 +0200

    Creating CsvLineParserTests

--- Unit Testing/WiredBrainCoffee.DataProcessor/Model/MachineDataItem.cs
+++ Unit Testing/WiredBrainCoffee.DataProcessor/Model/MachineDataItem.cs
@@ -1,15 +1,4 @@
 ﻿namespace WiredBrainCoffee.DataProcessor.Model
 {
-    public class MachineDataItem
-    {
-        public MachineDataItem(string coffeeType, DateTime createdAt)
-        {
-            CoffeeType = coffeeType;
-            CreatedAt = createdAt;
-        }
-
-        public string CoffeeType { get; }
-
-        public DateTime CreatedAt { get; }
-    }
+    public record MachineDataItem(string CoffeeType, DateTime CreatedAt);
 }
---
+++ Unit Testing/WiredBrainCoffee.DataProcessorTests/Parsing/CsvLineParserTests.cs
@@ -0,0 +1,18 @@
+﻿namespace WiredBrainCoffee.DataProcessor.Parsing
+{
+    public class CsvLineParserTests
+    {
+        [Fact]
+        public void ShouldParseValidLine()
+        {
+            string[] csvLines = new[] { "Cappuccino; 10 / 27 / 2022 8:06:04 AM" };
+
+            var machineDataItems = CsvLineParser.Parse(csvLines);
+
+            Assert.NotNull(machineDataItems);
+            Assert.Single(machineDataItems);
+            Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
+            Assert.Equal(new DateTime(2022,10,27,8,6,4), machineDataItems[1].CreatedAt);
+        }
+    }
+}
