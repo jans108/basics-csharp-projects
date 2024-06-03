f598dab55c83aee034347515599b5a8bc797a1c6
Maciej Broda <maciejbroda01@gmail.com>
2024-06-03 22:06:57 +0200

    Write Test to Skip Empty Lines or Whitespace

--- Unit Testing/WiredBrainCoffee.DataProcessorTests/Parsing/CsvLineParserTests.cs
+++ Unit Testing/WiredBrainCoffee.DataProcessorTests/Parsing/CsvLineParserTests.cs
@@ -1,18 +1,28 @@
-﻿namespace WiredBrainCoffee.DataProcessor.Parsing
+﻿namespace WiredBrainCoffee.DataProcessor.Parsing;
+
+public class CsvLineParserTests
 {
-    public class CsvLineParserTests
+    [Fact]
+    public void ShouldParseValidLine()
     {
-        [Fact]
-        public void ShouldParseValidLine()
-        {
-            string[] csvLines = new[] { "Cappuccino; 10 / 27 / 2022 8:06:04 AM" };
+        string[] csvLines = new[] { "Cappuccino; 10 / 27 / 2022 8:06:04 AM" };
 
-            var machineDataItems = CsvLineParser.Parse(csvLines);
+        var machineDataItems = CsvLineParser.Parse(csvLines);
 
-            Assert.NotNull(machineDataItems);
-            Assert.Single(machineDataItems);
-            Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
-            Assert.Equal(new DateTime(2022,10,27,8,6,4), machineDataItems[1].CreatedAt);
-        }
+        Assert.NotNull(machineDataItems);
+        Assert.Single(machineDataItems);
+        Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
+        Assert.Equal(new DateTime(2022,10,27,8,6,4), machineDataItems[0].CreatedAt);
+    }
+
+    [Fact]
+    public void ShouldSkipEmptyLines()
+    {
+        string[] csvLines = new[] { "", "  " };
+
+        var machineDataItems = CsvLineParser.Parse(csvLines);
+
+        Assert.NotNull(machineDataItems);
+        Assert.Empty(machineDataItems);
     }
 }
