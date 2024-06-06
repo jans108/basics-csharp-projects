b55d3f22dba8686c18f5b20698dae1cf81a4a7d1
Maciej Broda <maciejbroda01@gmail.com>
2024-06-06 22:04:49 +0200

    Test the MachineDataProcessor

---
+++ Unit Testing/WiredBrainCoffee.DataProcessorTests/Processing/MachineDataProcessorTests.cs
@@ -0,0 +1,25 @@
+﻿using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Processing;
+
+public class MachineDataProcessorTests
+{
+    [Fact]
+    public void ShouldSaveCountPerCoffeType()
+    {
+        //Arrange
+        var machineDataProcessor = new MachineDataProcessor();
+        var items = new[]
+        {
+            new MachineDataItem("Cappuccino",new DateTime(2022,10,27,8,0,0)),
+            new MachineDataItem("Cappuccino",new DateTime(2022,10,27,9,0,0)),
+            new MachineDataItem("Espresso",new DateTime(2022,10,27,10,0,0))
+        };
+
+        //Act
+        machineDataProcessor.ProcessItems(items);
+
+        //Assert
+
+    }
+}
