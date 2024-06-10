f37c98632cfef32acb0b3bc6fbc3563fba703b42
Maciej Broda <maciejbroda01@gmail.com>
2024-06-10 23:26:32 +0200

    Write a Unit Test

--- Unit Testing/WiredBrainCoffee.DataProcessorTests/Processing/MachineDataProcessorTests.cs
+++ Unit Testing/WiredBrainCoffee.DataProcessorTests/Processing/MachineDataProcessorTests.cs
@@ -62,6 +62,35 @@ public class MachineDataProcessorTests : IDisposable
         }
     }
 
+    [Fact]
+    public void ShouldIgnoreItemsThatAreNotNewer()
+    {
+        //Arrange
+        var items = new[]
+        {
+            new MachineDataItem("Cappuccino",new DateTime(2022,10,27,8,0,0)),
+            new MachineDataItem("Cappuccino",new DateTime(2022,10,27,7,0,0)),
+            new MachineDataItem("Cappuccino",new DateTime(2022,10,27,7,10,0)),
+            new MachineDataItem("Cappuccino",new DateTime(2022,10,27,9,0,0)),
+            new MachineDataItem("Espresso",new DateTime(2022,10,27,10,0,0)),
+            new MachineDataItem("Espresso",new DateTime(2022,10,27,10,0,0))
+        };
+
+        //Act
+        _machineDataProcessor.ProcessItems(items);
+
+        //Assert
+        Assert.Equal(2, _coffeeCountStore.SavedItems.Count);
+
+        var item = _coffeeCountStore.SavedItems[0];
+        Assert.Equal("Cappuccino", item.CoffeeType);
+        Assert.Equal(2, item.Count);
+
+        item = _coffeeCountStore.SavedItems[1];
+        Assert.Equal("Espresso", item.CoffeeType);
+        Assert.Equal(1, item.Count);
+    }
+
     public void Dispose()
     {
         // This runs after every test
