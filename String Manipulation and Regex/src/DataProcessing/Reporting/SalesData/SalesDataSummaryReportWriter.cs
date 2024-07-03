22c602202627bbda0b8e6dca1c7863119d2239a2
Maciej Broda <maciejbroda01@gmail.com>
2024-07-03 21:04:10 +0200

    Formatting Types as Strings

--- String Manipulation and Regex/src/DataProcessing/Reporting/SalesData/SalesDataSummaryReportWriter.cs
+++ String Manipulation and Regex/src/DataProcessing/Reporting/SalesData/SalesDataSummaryReportWriter.cs
@@ -28,6 +28,8 @@ internal class SalesDataSummaryReportWriter : DataWriter<IEnumerable<HistoricalS
 
         foreach (var item in salesData.OrderBy(d => d.UtcSalesDateTime))
         {
+            formattedOutput += "Date: " + item.UtcSalesDateTime.ToString("D", Options.ApplicationCulture) +
+                Environment.NewLine;
             formattedOutput += "Product Name: " + item.ProductName +
                 Environment.NewLine;
             formattedOutput += "Product SKU: " + item.ProductSku +
