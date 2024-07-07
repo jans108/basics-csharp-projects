30f61021209ecc04f3b922da7cf65d52aca4e8db
Maciej Broda <maciejbroda01@gmail.com>
2024-07-07 23:18:20 +0200

    String Interpolation

--- String Manipulation and Regex/src/DataProcessing/Reporting/SalesData/SalesDataSummaryReportWriter.cs
+++ String Manipulation and Regex/src/DataProcessing/Reporting/SalesData/SalesDataSummaryReportWriter.cs
@@ -1,4 +1,6 @@
-﻿namespace DataProcessing.Reporting;
+﻿using System.Runtime.CompilerServices;
+
+namespace DataProcessing.Reporting;
 
 internal class SalesDataSummaryReportWriter : DataWriter<IEnumerable<HistoricalSalesData>>
 {
@@ -7,8 +9,8 @@ internal class SalesDataSummaryReportWriter : DataWriter<IEnumerable<HistoricalS
     }
 
     protected override async Task WriteAsyncCore(
-        string pathAndFileName, 
-        IEnumerable<HistoricalSalesData> data, 
+        string pathAndFileName,
+        IEnumerable<HistoricalSalesData> data,
         CancellationToken cancellationToken = default)
     {
         var formattedOutput = ProduceReportString(data, cancellationToken);
@@ -20,21 +22,40 @@ internal class SalesDataSummaryReportWriter : DataWriter<IEnumerable<HistoricalS
     }
 
     private string ProduceReportString(
-        IEnumerable<HistoricalSalesData> salesData, 
+        IEnumerable<HistoricalSalesData> salesData,
         CancellationToken cancellationToken = default)
     {
-        var formattedOutput = "Data exported by " + SessionContext.Forename + " " +
-            SessionContext.Surname + Environment.NewLine;
+        //var formattedOutput = "Data exported by " + SessionContext.Forename + " " +
+        //    SessionContext.Surname + Environment.NewLine;
+
+        var formattedOutput = $"Date exoirted by {SessionContext.Forename} {SessionContext.Surname}{Environment.NewLine}";
+
+        //var interpolatedStringHandler = new DefaultInterpolatedStringHandler(18, 3);
+        //interpolatedStringHandler.AppendLiteral("Data exported by ");
+        //interpolatedStringHandler.AppendFormatted(SessionContext.Forename);
+        //interpolatedStringHandler.AppendLiteral(" ");
+        //interpolatedStringHandler.AppendFormatted(SessionContext.Surname);
+        //interpolatedStringHandler.AppendFormatted(Environment.NewLine);
+        //interpolatedStringHandler.ToStringAndClear();
 
         foreach (var item in salesData.OrderBy(d => d.UtcSalesDateTime))
         {
             //formattedOutput += "Date: " + item.UtcSalesDateTime.ToString("D", Options.ApplicationCulture) +
             //    Environment.NewLine;
-            formattedOutput += string.Format(Options.ApplicationCulture, "Date: {0:D}{1}", item.UtcSalesDateTime,  Environment.NewLine);
-            formattedOutput += "Product Name: " + item.ProductName +
-                Environment.NewLine;
-            formattedOutput += "Product SKU: " + item.ProductSku +
-                Environment.NewLine;
+            //formattedOutput += string.Format(Options.ApplicationCulture, "Date: {0:D}{1}", item.UtcSalesDateTime,  Environment.NewLine);
+            //formattedOutput += "Product Name: " + item.ProductName +
+            //    Environment.NewLine;
+            //formattedOutput += "Product SKU: " + item.ProductSku +
+            //    Environment.NewLine;
+
+            formattedOutput += $"Date: {item.UtcSalesDateTime.ToString("D", Options.ApplicationCulture)}{Environment.NewLine}";
+
+            //FormattableString dateString = $"Date: {item.UtcSalesDateTime:D}{Environment.NewLine}";
+            //formattedOutput += dateString.ToString(Options.ApplicationCulture);
+
+            formattedOutput += $"Product Name: {item.ProductName:D}{Environment.NewLine}";
+            formattedOutput += $"Product SKU: {item.ProductSku:D}{Environment.NewLine}";
+
         }
 
 
