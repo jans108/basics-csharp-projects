db5bd9e67d14ec8ffeaa44dee144e1c23b8c561f
Maciej Broda <maciejbroda01@gmail.com>
2024-09-05 21:44:32 +0200

    Fluent Coding

--- C# Playbook/Methods and Properties/Program.cs
+++ C# Playbook/Methods and Properties/Program.cs
@@ -1,9 +1,18 @@
 ﻿using Pluralsight.CShPlaybook.MethodsProps;
 
-CookieCustomer customer = new(0, "");
+CookieCustomer jimmy = new(1, "Jimmy");
+CookieCustomer rock = new(2, "Rock");
+CookieCustomer jack = new(3, "Jack");
 
-Console.WriteLine(customer);
+SalesList sales = new();
+sales.AddSale(new(jimmy, 200))
+    .AddSale(new(rock, 150))
+    .AddSale(new(jack, 400))
+    .AddSale(new(jimmy, 600))
+    .AddSale(new(rock, 50))
+    .AddSale(new(jack, 1000));
 
-char firstCh = customer.NameFirstChar;
-Console.WriteLine("Customer's name starts with " + firstCh);
-
+// Fluent coding in LINQ
+var highValueSales = sales.EnumerateItems()
+    .Where(s => s.Value > 100)
+    .OrderBy(s => s.Customer.Name)
---
+++ C# Playbook/Methods and Properties/Sale.cs
@@ -0,0 +1,17 @@
+﻿namespace Pluralsight.CShPlaybook.MethodsProps;
+
+public class Sale
+{
+    public CookieCustomer Customer { get; }
+
+    public decimal Value { get; }
+
+    public Sale(CookieCustomer customer, decimal amount)
+    {
+        if (customer == null)
+            throw new ArgumentNullException("A sale cannot have a null customer", nameof(customer));
+        Customer = customer;
+        Value = amount;
+    }
+}
+
