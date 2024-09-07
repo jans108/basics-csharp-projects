6a49d4a4b79a640ee7464d292e4c66e96c680655
Maciej Broda <maciejbroda01@gmail.com>
2024-09-07 22:38:22 +0200

    Returning Multiple Values with out Parameters

--- C# Playbook/Methods and Properties/Program.cs
+++ C# Playbook/Methods and Properties/Program.cs
@@ -12,6 +12,16 @@ sales.AddSale(new(jimmy, 200))
     .AddSale(new(rock, 50))
     .AddSale(new(jack, 1000));
 
+(string name, decimal totalValue, int NSales) = sales.GetHighestValueCustomer();
+
+Console.WriteLine("Highest spender: ");
+Console.WriteLine($"{name} Spent {totalValue} in {NSales} purchases");
+
+var highest = sales.GetHighestValueCustomer();
+
+Console.WriteLine("\r\nHighest spender: ");
+Console.WriteLine($"{highest.CustomerName} Spent {highest.TotalSpent} in {highest.NSales} pruchases");
+
 // Fluent coding in LINQ
 var highValueSales = sales.EnumerateItems()
     .Where(s => s.Value > 100)
