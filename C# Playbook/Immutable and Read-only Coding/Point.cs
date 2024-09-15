efcda86739488f755f889fb1fa4fbca3bad46b8e
Maciej Broda <maciejbroda01@gmail.com>
2024-09-15 17:05:42 +0200

    Making Structs Immutable

---
+++ C# Playbook/Immutable and Read-only Coding/Point.cs
@@ -0,0 +1,10 @@
+﻿namespace Pluralsight.CShPlaybook.DrawingStuff;
+
+public record struct Point(int X, int Y)
+{
+    public int DistSqFrom(Point other)
+        => (X - other.X) * (X - other.X) + (Y - other.Y) * (Y - other.Y);
+
+    public double DistFrom(Point other) => Math.Sqrt(DistSqFrom(other));
+    public override string ToString() => $"({X}, {Y})";
+}
