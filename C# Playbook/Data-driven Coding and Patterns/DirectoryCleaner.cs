370b71a192598c1f98877b3baf43cf6453cccbe7
Maciej Broda <maciejbroda01@gmail.com>
2024-09-16 20:08:17 +0200

    Improving the Code with a Switch Expression

--- C# Playbook/Data-driven Coding and Patterns/DirectoryCleaner.cs
+++ C# Playbook/Data-driven Coding and Patterns/DirectoryCleaner.cs
@@ -1,36 +1,33 @@
 ﻿namespace Pluralsight.CShDemo.ControlFlow;
 
-public enum FileAction { Backup, Ignore, Delete}
+public enum FileAction { Backup, Ignore, Delete }
 
 public static class DirectoryCleaner
 {
-	public static void DoBackupAndClean(DirectoryInfo targetDirectory)
-	{
-		foreach (System.IO.FileSystemInfo item in targetDirectory.EnumerateFileSystemInfos())
-			BackupAndCleanItem(item);
+    public static void DoBackupAndClean(DirectoryInfo targetDirectory)
+    {
+        foreach (System.IO.FileSystemInfo item in targetDirectory.EnumerateFileSystemInfos())
+        {
+            FileAction action = GetItemAction(item);
+            ProcessItem(action, item);
+        }
 
-		// In a real app you'd recursively clean sub-directories too
-	}
+        // In a real app you'd recursively clean sub-directories too
+    }
 
-	private static void ProcessItem(FileAction fileAction, FileSystemInfo item) =>
-		Console.WriteLine($"{fileAction}: {item.Name}");
+    private static void ProcessItem(FileAction fileAction, FileSystemInfo item) =>
+        Console.WriteLine($"{fileAction}: {item.Name}");
 
-	private static void BackupAndCleanItem(FileSystemInfo fileSystemItem)
-	{
-		const int MB100 = 104_857_600;
-		switch(fileSystemItem)
-		{
-			case DirectoryInfo { Name: "bin" or "obj" }:
-				ProcessItem(FileAction.Delete, fileSystemItem);
-				break;
-			case DirectoryInfo { Name: ".hg" }:
-			case FileInfo { Length: > MB100 }:
-			case FileInfo { Name: "TemporaryFile.tmp" }:
-				ProcessItem(FileAction.Ignore, fileSystemItem);
-				break;
-			default:
-				ProcessItem(FileAction.Backup, fileSystemItem);
-				break;
-		};
-	}
+    public static FileAction GetItemAction(FileSystemInfo fileSystemItem)
+    {
+        const int MB100 = 104_857_600;
+        return fileSystemItem switch
+        {
+            DirectoryInfo { Name: "bin" or "obj" } => FileAction.Delete,
+            DirectoryInfo { Name: ".hg" } => FileAction.Ignore,
+            FileInfo { Length: > MB100 } => FileAction.Ignore,
+            FileInfo { Name: "TemporaryFile.tmp" } => FileAction.Ignore,
+            _ => FileAction.Backup
+        };
+    }
 }
