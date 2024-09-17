4e0a94c5e0dc2381d50ce2a5c6c20f3c7e5a61d1
Maciej Broda <maciejbroda01@gmail.com>
2024-09-17 13:22:13 +0200

    Using INotifyPropertyChanged

--- C# Playbook/Events/BookmarkProduct.cs
+++ C# Playbook/Events/BookmarkProduct.cs
@@ -1,33 +1,47 @@
 ﻿using System.ComponentModel;
 
 namespace Pluralsight.CShPlaybook.EventsDemo;
-public class BookmarkProduct
+public class BookmarkProduct : INotifyPropertyChanged
 {
-	public string Id { get; init; }
-    public string Name { get; init; }
+    public string Id { get; init; }
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (value != _name)
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+    }
+    
+    private decimal _price;
+    public decimal Price
+    {
+        get => _price;
+        set
+        {
+            if (value != _price)
+            {
+                _price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged(string propName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
 
-	private decimal _price;
-	public decimal Price
-	{
-		get => _price;
-		set
-		{
-			if (value != _price)
-			{
-				var eventArgs = new PriceChangedEventArgs(_price);
-				_price = value;
-				PriceChanged?.Invoke(this, eventArgs);
-			}
-		}
-	}
-	public event PriceChangedEventHandler? PriceChanged;
-	public BookmarkProduct(string id, string name, decimal price)
-	{
-		if (string.IsNullOrWhiteSpace(id) || string.IsNullOrWhiteSpace(name) || price < 0.01m)
-			throw new ArgumentException("Invalid product details");
-		Id = id;
-		Name = name;
-		_price = price;
-	}
+    public BookmarkProduct(string id, string name, decimal price)
+    {
+        if (string.IsNullOrWhiteSpace(id) || string.IsNullOrWhiteSpace(name) || price < 0.01m)
+            throw new ArgumentException("Invalid product details");
+        Id = id;
+        _name = name;
+        _price = price;
+    }
 }
 
--- C# Playbook/Events/PriceAlerter.cs
+++ C# Playbook/Events/PriceAlerter.cs
@@ -3,18 +3,27 @@ namespace Pluralsight.CShPlaybook.EventsDemo;
 
 public class PriceAlerter
 {
-	public static void AlertPriceChanged(object? sender, PriceChangedEventArgs e)
+	private static string GetMessage(BookmarkProduct product, string? propName)
+	{
+		return propName switch
+		{
+			nameof(BookmarkProduct.Price) => $"{product.Name}: Price changed to {product.Price}",
+			nameof(BookmarkProduct.Name) => $"{product.Id} has new name {product.Name}",
+			_ => "Error - unknown property name"
+		};
+	}
+	public static void AlertPriceChanged(object? sender, PropertyChangedEventArgs e)
 	{
 		BookmarkProduct product = (BookmarkProduct)sender!;
 		if (product.Price < 0)
 		{
 			Console.WriteLine($"{product.Name} is no longer for sale");
-			product.PriceChanged -= AlertPriceChanged;
+			product.PropertyChanged -= AlertPriceChanged;
 		}
 		else
 		{
-			Console.WriteLine($"{product.Name} changed from {e.OldPrice} to {product.Price}");
-			product.PriceChanged += AlertPriceChanged;
+			Console.WriteLine(GetMessage(product, e.PropertyName));
+			
 		}
 	}
 }
