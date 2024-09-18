099921ada393dc0bf6519643bd9b69752a1c463d
Maciej Broda <maciejbroda01@gmail.com>
2024-09-18 19:28:01 +0200

    Grouping When You Join Lists

--- C# Playbook/LINQ/Program.cs
+++ C# Playbook/LINQ/Program.cs
@@ -1,23 +1,32 @@
 ﻿using Pluralsight.CShPlaybook.LinqDemos;
 
 var students = ResultsRepository.EnumStudents();
-var results = ResultsRepository.EnumResults().Distinct(ExamResultEqualityComparer.Instance);
+var results = ResultsRepository.EnumResults()
+    .Distinct(ExamResultEqualityComparer.Instance)
+    .OrderBy(result => result.Subject);
 
-IEnumerable<(Student student, ExamResult result)> studentResults =
+IEnumerable<(Student student, IEnumerable<ExamResult>)> studentResults =
     from Student student in students
+    orderby student.Name
     join ExamResult result in results on student.AnonymousId equals result.StudentId
-    orderby student.Name, result.Subject
-    select (student, result);
+    into resultsByStudent
+    select (student, resultsByStudent);
 
-var studentResultsFluentSyntax = students
-    .Join(results, student => student.AnonymousId,
+var studentResultsFluent = students
+    .GroupJoin(results,
+    student => student.AnonymousId,
     result => result.StudentId,
     (student, result) => (student, result))
-    .OrderBy(tuple => tuple.student.Name)
-    .ThenBy(tuple => tuple.result.Subject);
+    .OrderBy(tuple => tuple.student.Name);
 
 
-foreach ((Student student, ExamResult result) in studentResults)
-    Console.WriteLine($"{student.Name.PadRight(10)}: {result.Mark:##}% in {result.Subject}");
+foreach ((Student student, IEnumerable<ExamResult> resultsForStudent) in studentResults) 
+{
+    Console.WriteLine(student.Name);
+    foreach (ExamResult result in resultsForStudent)
+        Console.WriteLine($"  {result.Mark:##}% in {result.Subject}");
+}
+
+
 
 Console.ReadLine();
