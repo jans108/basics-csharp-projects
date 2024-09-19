a5ee12f9b1b33dcde6242996cdcb7f1e856c1a2f
Maciej Broda <maciejbroda01@gmail.com>
2024-09-19 22:37:16 +0200

    Creating Custom LINQ Extension Methods

--- C# Playbook/LINQ/Program.cs
+++ C# Playbook/LINQ/Program.cs
@@ -1,29 +1,16 @@
 ﻿using Pluralsight.CShPlaybook.LinqDemos;
 
-var students = ResultsRepository.EnumStudents();
-var results = ResultsRepository.EnumResults()
-    .Distinct(ExamResultEqualityComparer.Instance);
+var students = ResultsRepository
+    .EnumStudents()
+    .Take(3)
+    .Thottle()
+    .Log()
+    .OrderBy(x => x.Name)
+    .ToList();
 
-IEnumerable<(Student, double)> studentAverages =
-    from student in students
-    join result in results on student.AnonymousId equals result.StudentId
-    into resultsByStudent
-    let avg = resultsByStudent.Average(x => x.Mark)
-    orderby avg descending
-    select (student, avg);
-
-var studentAveragesFluent = students
-    .GroupJoin(
-    results,
-    student => student.AnonymousId,
-    r => r.StudentId,
-    (s, r) => (s, r.Average(r => r.Mark)))
-    .OrderBy(t => t.s.Name);
-
-foreach ((Student student, double avg) in studentAverages)
-    Console.WriteLine($"{student.Name.PadRight(10)}: {avg:##.#}%");
-
-
+Console.WriteLine("\r\nDisplaying results:");
+foreach (Student student in students)
+    Console.WriteLine(student);
 
 
 Console.ReadLine();
