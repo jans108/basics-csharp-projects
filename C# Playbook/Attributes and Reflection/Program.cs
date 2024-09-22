f53f26b434d702711cb166ab0ef56116e57b04fb
Maciej Broda <maciejbroda01@gmail.com>
2024-09-22 13:47:22 +0200

    Marking a Method as Obsolete

--- C# Playbook/Attributes and Reflection/Program.cs
+++ C# Playbook/Attributes and Reflection/Program.cs
@@ -1,9 +1,9 @@
 ﻿using Pluralsight.CShPlaybook.AttribsReflection;
 
-string textTemplate = DataFileReader.ReadDataFile("ProductPromo.txt");
+string textTemplate = DataFileReader.ReadDataFile("ProductPromoV2.txt");
 TextGenerator generator = new TextGenerator(textTemplate);
 
 var product = new ComputerProduct("Deluxe gaming computer", ProductStatus.InStock, 999, "16GB", 8);
-string result = generator.GenerateText(product);
+string result = generator.GenerateTextV2(product);
 
 Console.WriteLine(result);
