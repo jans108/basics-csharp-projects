00d6f111ce0e14d3341457c594f2acdc724fa455
Maciej Broda <maciejbroda01@gmail.com>
2024-09-29 20:55:16 +0200

    Benchmark Our Code

--- Benchmarking and Profiling/Benchmarking techniques/Benchmarks/ProcessingBenchmarks.cs
+++ Benchmarking and Profiling/Benchmarking techniques/Benchmarks/ProcessingBenchmarks.cs
@@ -1,20 +1,44 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Jobs;
+using StockAnalyzer.Processor;
 
 namespace Benchmarks;
 
-[SimpleJob(RunStrategy.ColdStart, RuntimeMoniker.Net80, iterationCount: 10)]
+[SimpleJob]
 [MemoryDiagnoser]
 public class ProcessingBenchmarks
 {
-
-    [Benchmark(Baseline = true)]
-    public string Calculate()
+    [GlobalSetup]
+    public void Setup()
     {
-        Thread.Sleep(Random.Shared.Next(500, 1000));
-
-        return "Avoid JIT elimination";
+        // we can do some methods without benchmarking. Depends on our goal.
     }
 
+    [Benchmark]
+    public List<string> Processor()
+    {
+        var result = new List<string>();
+
+        var processor = new Processor();
+
+        processor.Initialize();
+
+        foreach (var stock in processor.Stocks)
+        {
+            var min = processor.Min(stock.Key);
+            var max = processor.Max(stock.Key);
+            var avarage = processor.Average(stock.Key);
+
+            result.Add($"{min} {max} {avarage}");
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public List<string> ProcessorFaster()
+    {
+        return [];
+    }
 }
