7e3e9c7e1c0ee2c2e5923a237ece3cd2bdeea197
Maciej Broda <maciejbroda01@gmail.com>
2024-09-30 18:41:18 +0200

    Init app

---
+++ Benchmarking and Profiling/Profiling for Performance Analysis/StockAnalyzer.App/Program.cs
@@ -0,0 +1,62 @@
+﻿using StockAnalyzer.Processor;
+using System.Diagnostics;
+
+var processor = new Processor();
+var watch = new Stopwatch();
+
+while (true)
+{
+    Console.WriteLine("1 - Run processor");
+    Console.WriteLine("0 - Exit");
+
+    Console.Write("What do you want to do: ");
+    var action = Console.ReadLine()?.Trim();
+
+    if (action is not "1" or null)
+    {
+        Console.WriteLine("Exiting");
+        break;
+    }
+
+    watch.Restart();
+
+    RunProcessor();
+
+    // Task.Run(Deadlock);
+
+    Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms{Environment.NewLine}");
+}
+
+void Deadlock()
+{
+    Task t2 = null!;
+
+    var t1 = Task.Run(RunProcessor).ContinueWith(t =>
+    {
+        t2.Wait();
+    });
+
+    t2 = Task.Run(() =>
+    {
+        t1.Wait();
+    });
+}
+
+void RunProcessor()
+{
+    var result = string.Empty;
+
+    Console.WriteLine("Starting..");
+
+    processor.Initialize();
+
+    foreach (var stock in processor.Stocks)
+    {
+        var min = processor.Min(stock.Key);
+        var max = processor.Max(stock.Key);
+        var average = processor.Average(stock.Key);
+
+        result += $"{stock.Key},{min},{max},{average}{Environment.NewLine}";
+    }
+    File.WriteAllText("Result.txt", $"{result}");
+}
