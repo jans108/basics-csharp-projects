21de1aa7d178e8bd3bcdacb44a4097058ea40cca
Maciej Broda <maciejbroda01@gmail.com>
2024-10-04 12:28:43 +0200

    Avoid Flow Control Using Exceptions

---
+++ Benchmarking and Profiling/Optimize Memory and CPU Usage - Best Practices/Benchmarks/FlowControlBenchmarker.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks;
+
+[SimpleJob]
+[MemoryDiagnoser]
+public class FlowControlBenchmarker
+{
+    [Benchmark]
+    public decimal ProcessWithoutException()
+    {
+        decimal result = 0;
+
+        for (int i = 0; i < 10000; i++)
+        {
+            var dimensions = GetDimensionsFor(i);
+
+            if(dimensions is not null)
+            {
+                result += dimensions.Width;
+            }
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public decimal ProcessWithException()
+    {
+        decimal result = 0;
+
+        for (int i = 0; i < 10000; i++)
+        {
+            try
+            {
+                var dimensions = GetDimensionsFor(i);
+                result += dimensions.Width;
+            }
+            catch
+            {
+
+            }
+        }
+
+        return result;
+    }
+
+    public Dimensions GetDimensionsFor(int index)
+    {
+        if (index % 10 == 0)
+            return null!;
+        return new(100, 100);
+    }
+
+    public record Dimensions(int Width, int Height);
+}
