56b940efdb24afa73cddb3216bb157eca4ec27c0
Maciej Broda <maciejbroda01@gmail.com>
2024-10-16 20:55:54 +0200

    Demonstrating Variance

--- Collection and Generics in C#/Using Generic types in the object model slides/ConsoleDemo/Program.cs
+++ Collection and Generics in C#/Using Generic types in the object model slides/ConsoleDemo/Program.cs
@@ -1,5 +1,17 @@
-﻿try
+﻿using ConsoleDemo;
+
+try
 {
+    IEnumerable<Worker> workers =
+        Workers.TestData.Take(5).Replicate(2, .1F).BeginShuffle().Iterate().ToList();
+
+    IEnumerable<Employee> employees = workers.Concat(
+        workers.Take(5).Select(worker => new Employee(worker.Name)))
+        .BeginShuffle().Iterate().ToList();
+
+    List<Employee> items = new(employees);
+    items.Sort(Employee.DefaultEmployeeComparer);
+    employees.ToGrid(120, 2).WriteLines();
 }
 catch (Exception e)
 {
