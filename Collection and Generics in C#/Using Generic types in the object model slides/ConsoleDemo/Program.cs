5e4d3c536cddb01632e649d0cffafd20b318217a
Maciej Broda <maciejbroda01@gmail.com>
2024-10-17 21:13:48 +0200

    Designing a Convariant Generic Type

--- Collection and Generics in C#/Using Generic types in the object model slides/ConsoleDemo/Program.cs
+++ Collection and Generics in C#/Using Generic types in the object model slides/ConsoleDemo/Program.cs
@@ -2,16 +2,7 @@
 
 try
 {
-    IEnumerable<Worker> workers =
-        Workers.TestData.Take(5).Replicate(2, .1F).BeginShuffle().Iterate().ToList();
-
-    IEnumerable<Employee> employees = workers.Concat(
-        workers.Take(5).Select(worker => new Employee(worker.Name)))
-        .BeginShuffle().Iterate().ToList();
-
-    List<Employee> items = new(employees);
-    items.Sort(Employee.DefaultEmployeeComparer);
-    employees.ToGrid(120, 2).WriteLines();
+    
 }
 catch (Exception e)
 {
---
+++ Collection and Generics in C#/Using Generic types in the object model slides/Models/Collections/IOrderedList.cs
@@ -0,0 +1,6 @@
+﻿namespace Models.Collections;
+
+public interface IOrderedList<T> : IReadOnlyList<T>
+{
+    IOrderedList<T> GetRange(int index, int count);
+}
