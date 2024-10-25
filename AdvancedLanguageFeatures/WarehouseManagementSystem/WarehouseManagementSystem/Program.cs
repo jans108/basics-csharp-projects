f4d56171794cd8a2ce8e192f28bbd876523ca8fe
Maciej Broda <maciejbroda01@gmail.com>
2024-10-25 11:31:01 +0200

    Multicast Delegate and Chains

--- AdvancedLanguageFeatures/WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
+++ AdvancedLanguageFeatures/WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
@@ -18,16 +18,27 @@ var processor = new OrderProcessor()
     OnOrderInitialized = SendMessageToWarehouse
 };
 
-processor.Process(order, SendConfirmationEmail);
+OrderProcessor.ProcessCompleted chain = (OrderProcessor.ProcessCompleted)One + Two + Three;
+
+
+processor.Process(order, chain);
+
+chain -= Two;
+
+processor.Process(order, chain);
 
 bool SendMessageToWarehouse(Order order)
 {
     Console.WriteLine($"Please pack the order {order.OrderNumber}");
 
-    return false;
+    return true;
 }
 
 void SendConfirmationEmail(Order order)
 {
     Console.WriteLine($"Order Confirmation Email for {order.OrderNumber}");
 }
+
+void One(Order order) => Console.WriteLine("One");
+void Two(Order order) => Console.WriteLine("Two");
+void Three(Order order) => Console.WriteLine("Three");
