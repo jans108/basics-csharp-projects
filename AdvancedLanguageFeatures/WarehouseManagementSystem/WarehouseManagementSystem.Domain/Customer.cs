422270353c1f4e43bc125a69a5926bc9e1281822
Maciej Broda <maciejbroda01@gmail.com>
2024-11-12 17:45:23 +0100

    Adding Additional Members to a Record

--- AdvancedLanguageFeatures/WarehouseManagementSystem/WarehouseManagementSystem.Domain/Customer.cs
+++ AdvancedLanguageFeatures/WarehouseManagementSystem/WarehouseManagementSystem.Domain/Customer.cs
@@ -1,6 +1,13 @@
 ﻿namespace WarehouseManagementSystem.Domain
 {
-    public record Customer(string Firstname, string Lastname);
+    public record Customer(string Firstname, string Lastname)
+    {
+        public string Fullname => $"{Firstname} {Lastname}";
+
+        public Address Address { get; init; }
+    }
+
+    public record Address(string street, string postalCode);
     
     public record PriorityCustomer(string Firstname, string Lastname) : Customer(Firstname, Lastname);
 }
